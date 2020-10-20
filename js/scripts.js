b9a41108f8f2cbcd899783c1ca8fe942705d9968
Tawnee Harris <tawneeh@icloud.com>
2020-10-20 14:59:10 -0700

    make all cards clickable with front and back content
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
-  $(".card-title").click(function() {
-    $(".term-showing").toggle();
-    $(".definition-showing").toggle();
+  $(".card1").click(function() {
+    $(".front1").toggle();
+    $(".back1").toggle();
+  });
+
+  $(".card2").click(function() {
+    $(".front2").toggle();
+    $(".back2").toggle();
+  });
+
+  $(".card3").click(function() {
+    $(".front3").toggle();
+    $(".back3").toggle();
+  });
+
+  $(".card4").click(function() {
+    $(".front4").toggle();
+    $(".back4").toggle();
   });
 
   //#term-showing[1] 
+
+//$ function clicking(".card-titile").click(function()) {
+  //$(".term-showing").toggle();
+  //$(".definition-showing").toggle();
+//});
+
+//clicking(".clickHere1");
 
 });
