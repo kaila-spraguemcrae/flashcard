f6259ae3d87e1e7044b450f60cfb96335209006b
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-20 22:22:06 -0700

    add js to all flashcards and add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,88 @@ $(document).ready(function() {
     $(".back4").toggle();
   });
 
+  $(".card5").click(function() {
+    $(".front5").toggle();
+    $(".back5").toggle();
+  });
+
+  $(".card6").click(function() {
+    $(".front6").toggle();
+    $(".back6").toggle();
+  });
+
+  $(".card7").click(function() {
+    $(".front7").toggle();
+    $(".back7").toggle();
+  });
+
+  $(".card8").click(function() {
+    $(".front8").toggle();
+    $(".back8").toggle();
+  });
+
+  $(".card9").click(function() {
+    $(".front9").toggle();
+    $(".back9").toggle();
+  });
+
+  $(".card10").click(function() {
+    $(".front10").toggle();
+    $(".back10").toggle();
+  });
+
+  $(".card11").click(function() {
+    $(".front11").toggle();
+    $(".back11").toggle();
+  });
+
+  $(".card12").click(function() {
+    $(".front12").toggle();
+    $(".back12").toggle();
+  });
+
+  $(".card13").click(function() {
+    $(".front13").toggle();
+    $(".back13").toggle();
+  });
+
+  $(".card14").click(function() {
+    $(".front14").toggle();
+    $(".back14").toggle();
+  });
+
+  $(".card15").click(function() {
+    $(".front15").toggle();
+    $(".back15").toggle();
+  });
+
+  $(".card16").click(function() {
+    $(".front16").toggle();
+    $(".back16").toggle();
+  });
+
+  $(".card17").click(function() {
+    $(".front17").toggle();
+    $(".back17").toggle();
+  });
+
+  $(".card18").click(function() {
+    $(".front18").toggle();
+    $(".back18").toggle();
+  });
+
+  $(".card19").click(function() {
+    $(".front19").toggle();
+    $(".back19").toggle();
+  });
+
+  //$(".card20").click(function() {
+  // $(".front20").toggle();
+  //  $(".back20").toggle();
+ // });
+
+
+
   //#term-showing[1] 
 
 //$ function clicking(".card-titile").click(function()) {
